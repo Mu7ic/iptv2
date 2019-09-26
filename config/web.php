e9f889c52a167c450ca0ddbdbeaf229681417c26
Muzich <va7tal@mail.ru>
2019-09-26 09:21:39 +0500

    .htaccess

--- config/web.php
+++ config/web.php
@@ -21,7 +21,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '0dRiYXydyy9jTa7p7zFes9-WNpbcyroH',
-            'enableCsrfValidation' => false,
+            //'enableCsrfValidation' => false,
             //'baseUrl' => ''
         ],
         'cache' => [
@@ -67,7 +67,7 @@ $config = [
 
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => true,
+            'showScriptName' => false,
             'rules' => [
                 'chanels'=>'site/index',
                 'chanel/<id:\w+>'=>'site/single',
